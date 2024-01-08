3b18fc81f661702b31d2975ba730a49e205e3d81
maruands <maruands@gmail.com>
2024-01-08 23:49:58 +0300

    fill in memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_memset - function that fills memory with a constant byte 
+*_memset - function that fills memory with a constant byte
 *@s first value
 *@b second value
 *@n third value
@@ -11,10 +11,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int j;
 
-	for(j=0; j<n; j++)
+	for (j = 0; j < n; j++)
 	{
 		s[j] = b;
 	}
-	return(s);	
+	return (s);
 }
 
