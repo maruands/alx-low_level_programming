a91c5676d9bebe5597052a31311fa6f92258546f
maruands <maruands@gmail.com>
2024-01-09 00:22:19 +0300

    copied memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_memcpy - fill in the memory with another dest
+*@dest: string to fill
+*@src: source string
+*@n: size of memory
+*Return: a pointer to dest
+**/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int k;
+
+	while (k < n)
+	{
+		*(dest + k) = *(src + k);
+		k++;
+	}
+	return (dest);
+}
